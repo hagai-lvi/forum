349620ab0e2b157676dc4a6979df289cdc99a601
hagai-lvi <hagai.lvi@gmail.com>
2015-04-06 12:37:17 +0300

    initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,7 @@
+package main;
+
+/**
+ * Created by hagai_lvi on 4/6/15.
+ */
+public class Main {
+}
---
+++ src/test/java/main_test/Main.java
@@ -0,0 +1,8 @@
+package main_test;
+
+/**
+ * Created by hagai_lvi on 4/6/15.
+ */
+public class Main {
+
+}
