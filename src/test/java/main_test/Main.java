9084cc27d19d249d87088cf3b9a86a2850757560
hagai-lvi <hagai.lvi@gmail.com>
2015-04-06 13:25:59 +0300

    Added test template

--- src/test/java/main_test/Main.java
+++ src/test/java/main_test/Main.java
@@ -1,10 +1,37 @@
 package main_test;
 
-import junit.framework.TestCase;
+import org.junit.*;
 
 /**
  * Created by hagai_lvi on 4/6/15.
  */
-public class Main extends TestCase {
+public class Main {
+
+	@BeforeClass
+	public static void beforeClass(){
+		System.out.println("Before class");
+	}
+
+	@Before
+	public void beforeTest(){
+		System.out.println("Before test");
+	}
+
+	@Test
+	public void test1(){
+		System.out.println("Test");
+		Assert.assertTrue(true);
+	}
+
+	@After
+	public void afterTest(){
+		System.out.println("After test");
+
+	}
+
+	@AfterClass
+	public static void afterClass(){
+		System.out.println("After class");
+	}
 
 }
