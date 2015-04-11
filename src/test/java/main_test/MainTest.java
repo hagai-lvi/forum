472c18c16beea9ab6b727eb899c1f5f8cf1544b9
Gila <gilmor89@gmail.com>
2015-04-11 15:58:32 +0300

    added maintest

--- src/test/java/main_test/MainTest.java
+++ src/test/java/main_test/MainTest.java
@@ -1,25 +1,36 @@
 package main_test;
 
 import org.junit.*;
+import main.interfaces.*;
+import main.exceptions.*;
+import main.forum_contents.*;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Created by hagai_lvi on 4/6/15.
  */
 public class MainTest {
+	Facade _facade;
+	Collection<ForumI> _forumCollection;
+	HashMap<ForumI,UserI> _forumUsers;
 
 	public static Logger logger = Logger.getLogger(MainTest.class.getName());
 
 
 	@BeforeClass
 	public static void beforeClass(){
+		//logger.setLevel(Level.FINE);
+
 		System.out.println("Before class");
 	}
 
 	@Before
 	public void beforeTest(){
-		System.out.println("Before test");
+		logger.fine("123");
 	}
 
 	@Test
