37898f0dddc7b408383a99d27162ffd96afe9b78
gabi-giladov <gabi.giladov@gmail.com>
2015-04-11 16:17:40 +0300

    new Class added: User implements UserI interface

---
+++ src/test/java/main/User/UserPermissionTest.java
@@ -0,0 +1,48 @@
+package main.User;
+
+import main.interfaces.UserI;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gabigiladov on 4/11/15.
+ */
+public class UserPermissionTest {
+
+    @Before
+    public void setUp() throws Exception {
+        UserI user = new User("Gabi", "123456");
+    }
+
+    @Test
+    public void testCreateSubForum() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteSubForum() throws Exception {
+
+    }
+
+    @Test
+    public void testCreatThread() throws Exception {
+
+    }
+
+    @Test
+    public void testReplyToMessage() throws Exception {
+
+    }
+
+    @Test
+    public void testReportModerator() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteMessage() throws Exception {
+
+    }
+}
