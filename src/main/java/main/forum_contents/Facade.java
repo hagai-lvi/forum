86c8e77d3b3f250ca47fa3658e1126d0d0b4cf16
hagai-lvi <hagai.lvi@gmail.com>
2015-04-11 16:34:04 +0300

    added empty facade

---
+++ src/main/java/main/forum_contents/Facade.java
@@ -0,0 +1,17 @@
+package main.forum_contents;
+
+import main.interfaces.FacadeI;
+import main.interfaces.ForumI;
+
+import java.util.Collection;
+
+/**
+ * Created by hagai_lvi on 4/11/15.
+ */
+public class Facade implements FacadeI {
+	@Override
+	public Collection<ForumI> getForumList() {
+		//TODO
+		return null;
+	}
+}
