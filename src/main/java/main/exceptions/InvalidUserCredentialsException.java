c206539af5ff0180023bf7395d4f727b6f818f4c
hagai-lvi <hagai.lvi@gmail.com>
2015-04-15 00:02:36 +0300

    fix exception

--- src/main/java/main/exceptions/InvalidUserCredentialsException.java
+++ src/main/java/main/exceptions/InvalidUserCredentialsException.java
@@ -5,5 +5,5 @@ package main.exceptions;
  * Created by hagai on 07/04/15.
  */
 //TODO add reason? like user not found, password doesn't match etc
-public class InvalidUserCredentialsException extends Throwable {
+public class InvalidUserCredentialsException extends Exception {
 }
