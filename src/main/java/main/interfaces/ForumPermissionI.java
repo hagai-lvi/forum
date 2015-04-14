323a4ab7a032c6bf7753ca464bc03117e2ff9c74
gabi-giladov <gabi.giladov@gmail.com>
2015-04-14 19:09:29 +0300

    getters and setter to User

--- src/main/java/main/interfaces/ForumPermissionI.java
+++ src/main/java/main/interfaces/ForumPermissionI.java
@@ -18,7 +18,7 @@ public interface ForumPermissionI {
 	/**
 	 * Delete a subForum from this forum
 	 */
-	void deleteSubForum(SubForumI toDelete) throws PermissionDenied;
+	void deleteSubForum(SubForumI toDelete) throws PermissionDeniedException;
 
 	/**
 	 * Set new forum administrator
