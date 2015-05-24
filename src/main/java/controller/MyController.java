1ffde29d4834c7fe5f3207c7e9d5dab7b30ae9c3
hagai-lvi <hagai.lvi@gmail.com>
2015-05-24 17:08:45 +0300

    start implementing controller

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -1,22 +1,18 @@
 package controller;
 
 //import data_structures.TreeNode;
-import main.User.User;
-import main.exceptions.*;
+
+import main.exceptions.ForumAlreadyExistException;
+import main.exceptions.PermissionDeniedException;
+import main.exceptions.SubForumAlreadyExistException;
 import main.interfaces.FacadeI;
-import main.interfaces.ForumI;
-import main.interfaces.SubForumPermissionI;
-import main.interfaces.UserI;
 import main.services_layer.Facade;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -24,12 +20,15 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 public class MyController {
-	public static final String SESSION_THREAD_ATTR = "thread";
 	private Logger logger = Logger.getLogger(this.getClass());
 
-	public static final String SESSION_USER_ATTR = "user";
-	public static final String SESSION_FORUM_ATTR = "forum";
-	public static final String SESSION_SUBFORUM_ATTR = "subforum";
+	public static final String SESSION_ID_ATTR = "session_id";
+
+
+	public static final String ADMIN_USER = "ADMIN";// TODO remove
+	public static final String ADMIN_PASS = "ADMIN";// TODO remove
+
+
 
 	/**
 	 * Shows a facade with all the available forums in the system
@@ -44,8 +43,10 @@ public class MyController {
 	 * Send a request to create a new forum in the system
 	 */
 	@RequestMapping(value = "/addForum",method = RequestMethod.POST)
-	public void addForum(ModelMap model, String forumName, int numOfModerators, String passRegex) {
-
+	public void addForum(ModelMap model, String forumName, int numOfModerators, String passRegex) throws PermissionDeniedException, ForumAlreadyExistException {
+		FacadeI f = Facade.getFacade();
+		f.addForum(ADMIN_PASS, ADMIN_USER, forumName, passRegex, numOfModerators); //TODO get credentials from the user
+		model.addAttribute("forumName", forumName);
 	}
 
 
@@ -54,6 +55,11 @@ public class MyController {
 	 */
 	@RequestMapping(value = "/addSubforum",method = RequestMethod.POST)
 	public void addSubforum(ModelMap model, HttpSession session, String subforumName) throws SubForumAlreadyExistException, PermissionDeniedException {
+//		FacadeI f = Facade.getFacade();
+//		UserI user = (UserI) session.getAttribute(SESSION_USER_ATTR);
+//		Integer sessionId = (Integer) session.getAttribute(SESSION_ID_ATTR);
+//		f.createSubforum(sessionId, subforumName);
+//		model.addAttribute("subforumName", subforumName);
 	}
 
 
@@ -62,182 +68,9 @@ public class MyController {
 	 */
 	@RequestMapping(value = "/login_page",method = RequestMethod.POST)
 	public void loginForm(ModelMap model, HttpSession session, String forum) {
+		model.addAttribute("forumName", forum);
+		FacadeI facade = Facade.getFacade();
 	}
-
-
-	/**
-	 * redirects to the current forum home page after a login
-	 */
-	@RequestMapping(value = "forum_homepage",method = RequestMethod.POST)
-	public String loginToForum(ModelMap model, HttpSession session, String username, String password) throws InvalidUserCredentialsException {
-		return "forum_homepage";
-	}
-
-	/**
-	 * Redirects to the forum home page, assumes that a user has already logged in
-	 */
-	@RequestMapping(value = "forum_homepage",method = RequestMethod.GET)
-	public String showForumHomepage(ModelMap model, HttpSession session) throws InvalidUserCredentialsException {
-		return "forum_homepage";
-	}
-
-	/**
-	 * Register a user to the current forum
-	 */
-	@RequestMapping(value = "register",method = RequestMethod.POST)
-	public String register(ModelMap model, HttpSession session, String username, String password, String email)
-			throws UserAlreadyExistsException, InvalidUserCredentialsException {
-		return "forum_homepage";
-	}
-
-	/**
-	 * Allows to clear all data
-	 */
-	@RequestMapping(value = "reset",method = RequestMethod.GET)
-	public String resetSystem(){
-		return "redirect:/facade";
-	}
-
-	private void preperaForumHomepageModel(ModelMap model, FacadeI facade, ForumI forum, UserI user) {
-	}
-
-
-	/**
-	 * redirects to the current forum home page after a login
-	 */
-	@RequestMapping(value = "subforum_homepage",method = RequestMethod.POST)
-	public String showSubforumHomepage(ModelMap model, HttpSession session, String subforumName) throws InvalidUserCredentialsException {
-		return "subforum_homepage";
-	}
-
-	/**
-	 * Called when the user is already logged in and the subforum is listed in the httpsession
-	 */
-	@RequestMapping(value = "subforum_homepage",method = RequestMethod.GET)
-	public String refreshSubforumHomepage(ModelMap model, HttpSession session) throws InvalidUserCredentialsException {
-		return "subforum_homepage";
-	}
-
-	private void preperaSubforumHomepageModel(ModelMap model, FacadeI facade, SubForumPermissionI subforum, User user) {
-	}
-
-
-	/**
-	 * Send a request to create a new sub forum in the currently used forum
-	 */
-	@RequestMapping(value = "/addThread",method = RequestMethod.POST)
-	public void addThread(ModelMap model, HttpSession session, String srcMsgTitle, String srcMsgBody) throws PermissionDeniedException, DoesNotComplyWithPolicyException {
-	}
-
-
-
-	/**
-	 * Called when the user is already logged in and the subforum is listed in the httpsession
-	 */
-	@RequestMapping(value = "thread_view",method = RequestMethod.GET)
-	public String showThread(ModelMap model, HttpSession session, long threadID){
-		return "thread_view";
-	}
-
-	@RequestMapping(value = "thread_view",method = RequestMethod.POST)
-	public String addMessageAndShowThread(ModelMap model, HttpSession session, String newMsgTitle, String newMsgBody, Long messageID) throws MessageNotFoundException, PermissionDeniedException, DoesNotComplyWithPolicyException {
-
-		return "thread_view";
-	}
-
-	//TODO remove, for testing only
-	@RequestMapping(value = "abc",method = RequestMethod.GET)
-	public static String showThread(ModelMap model, HttpServletRequest request){
-		return "thread_view";
-	}
-
-	@RequestMapping(value = "/reply_to_message",method = RequestMethod.POST)
-	public String addReplyMessage(ModelMap model, HttpSession session, Long messageID) throws PermissionDeniedException, DoesNotComplyWithPolicyException {
-//		FacadeI f = Facade.getFacade();
-//		UserI user = (UserI) session.getAttribute(SESSION_USER_ATTR);
-//		SubForumPermissionI subforum = (SubForumPermissionI)session.getAttribute(SESSION_SUBFORUM_ATTR);
-//		f.createNewThread(user, subforum, srcMsgTitle, srcMsgBody);
-//		model.addAttribute("threadTitle", srcMsgTitle);
-		model.addAttribute("messageID", messageID);
-		return "reply_to_message";
-	}
-
-
-	@ExceptionHandler(Exception.class)
-	public ModelAndView handleError(HttpServletRequest req, Exception exception) {
-		logger.error("Request: " + req.getRequestURL() + " raised an exception", exception);
-
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("error_message", exception.getMessage());
-		mav.addObject("exception", exception);
-		mav.addObject("url", req.getRequestURL());
-		mav.setViewName("error");
-		return mav;
-	}
-
-
-
-
-
-
-
-
-
-	///////////////////////////////////////////
-
-
-
-//
-//
-//	public static final String SESSION_THREAD_ATTR = "thread";
-//	private Logger logger = Logger.getLogger(this.getClass());
-//
-//	public static final String SESSION_USER_ATTR = "user";
-//	public static final String SESSION_FORUM_ATTR = "forum";
-//	public static final String SESSION_SUBFORUM_ATTR = "subforum";
-//
-//	/**
-//	 * Shows a facade with all the available forums in the system
-//	 */
-//	@RequestMapping(value = "/facade",method = RequestMethod.GET)
-//	public void showFacade(ModelMap model) {
-//		FacadeI f = Facade.getFacade();
-//		model.addAttribute("forumList", f.getForumList());
-//	}
-//
-//	/**
-//	 * Send a request to create a new forum in the system
-//	 */
-//	@RequestMapping(value = "/addForum",method = RequestMethod.POST)
-//	public void addForum(ModelMap model, String forumName, int numOfModerators, String passRegex) {
-//		FacadeI f = Facade.getFacade();
-//		ForumPolicyI policy = new ForumPolicy(numOfModerators, passRegex);
-//		f.addForum(new Forum(forumName, policy));
-//		model.addAttribute("forumName", forumName);
-//	}
-//
-//
-//	/**
-//	 * Send a request to create a new sub forum in the currently used forum
-//	 */
-//	@RequestMapping(value = "/addSubforum",method = RequestMethod.POST)
-//	public void addSubforum(ModelMap model, HttpSession session, String subforumName) throws SubForumAlreadyExistException, PermissionDeniedException {
-//		FacadeI f = Facade.getFacade();
-//		UserI user = (UserI) session.getAttribute(SESSION_USER_ATTR);
-//		f.createSubforum(subforumName, user);
-//		model.addAttribute("subforumName", subforumName);
-//	}
-//
-//
-//	/**
-//	 * Shows a login/register page
-//	 */
-//	@RequestMapping(value = "/login_page",method = RequestMethod.POST)
-//	public void loginForm(ModelMap model, HttpSession session, String forum) {
-//		model.addAttribute("forumName", forum);
-//		FacadeI facade = Facade.getFacade();
-//		session.setAttribute(SESSION_FORUM_ATTR, facade.getForumByName(forum));
-//	}
 //
 //
 //	/**
